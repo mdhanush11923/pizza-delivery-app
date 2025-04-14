05cc4fdd2535b1c7fb477e66c3d0bc1c45b9e4bd
M Dhanush <mdhanush11923@gmail.com>
2025-04-14 22:29:26 +0530

    make startpage responsive

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Button, Image } from "@heroui/react";
 import { siteConfig } from "@/config/site";
@@ -10,50 +10,61 @@ import { useRouter } from "next/navigation";
 import { BorderBeam } from "@/components/ui/border-beam";
 import Link from "next/link";
 
-
 export default function StartPage() {
   const router = useRouter();
+
   return (
     <div>
-      <section className="flex flex-wrap items-center justify-center gap-10 px-10 py-8 md:py-20 select-none">
-        <div className="max-w-lg gap-4">
-          <div className="flex gap-10">
-            <div className="flex select-none">
+      <section className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center justify-center gap-10 px-6 py-10 lg:py-24 select-none relative">
+        {/* === TEXT === */}
+        <div className="w-full lg:w-2/5 z-10 mt-[-15rem] lg:mt-0">
+          <div className="absolute inset-0 bg-white/60 dark:bg-black/60  backdrop-blur-md rounded-xl -z-10 lg:hidden" />
+          <div className="flex gap-10 items-center">
+            <div className="select-none flex">
               <p className="font-black text-inherit text-2xl">PIZzA</p>
               <p className="font-regular text-inherit">Delivery</p>
             </div>
-            <div className="">
-              <ThemeSwitch />
-            </div>
+            <ThemeSwitch />
           </div>
+
           <h1 className="scroll-m-20 mb-10 mt-10 font-black tracking-tight text-3xl lg:text-5xl">
             Delicious Pizza Delivered Right to Your Doorstep
           </h1>
+
           <h2 className="scroll-m-20 pb-2 text-lg tracking-tight mt-6 lg:text-xl">
             Craving pizza? We&apos;ve got you covered!{" "}
             <span
-              className={`${title({ color: "yellow", size: "vs" })} whitespace-nowrap`}
+              className={`${title({
+                color: "yellow",
+                size: "vs",
+              })} whitespace-nowrap`}
             >
               🔥Hot
             </span>
             {", "}
             <span
-              className={`${title({ color: "blue", size: "vs" })}  whitespace-nowrap`}
+              className={`${title({
+                color: "blue",
+                size: "vs",
+              })}  whitespace-nowrap`}
             >
               🍃fresh
             </span>{" "}
-            <span className={``}>and delivered</span>
+            and delivered{" "}
             <span
-              className={`${title({ color: "green", size: "vs" })} whitespace-nowrap`}
+              className={`${title({
+                color: "green",
+                size: "vs",
+              })} whitespace-nowrap`}
             >
               ⚡fast!
             </span>
           </h2>
 
-          <div className="flex flex-wrap gap-3 mt-14">
-            <div className="relative overflow-hidden w-full sm:w-44 rounded-lg">
+          <div className="flex flex-wrap gap-3 mt-14 ">
+            <div className="relative overflow-hidden w-full xm:w-44 max-w-80 rounded-lg">
               <Button
-                className="h-16 w-full sm:w-44 font-bold border-3 border-foreground text-foreground rounded-lg hover:font-extrabold"
+                className="h-16 w-full xm:w-44 font-bold border-3 border-foreground text-foreground rounded-lg hover:font-extrabold"
                 size="lg"
                 href="/login"
                 as={Link}
@@ -61,29 +72,27 @@ export default function StartPage() {
               >
                 Login
               </Button>
-              <BorderBeam
-                size={500}
-                duration={5}
-                borderWidth={3}
-                // colorFrom="#006BFF"
-                // colorTo="#FF9400"
-              />
+              <BorderBeam size={500} duration={5} borderWidth={3} />
             </div>
 
             <RainbowButton onClick={() => router.push("/signup")}>
-              <p className=" text-background">Sign up</p>
+              <p className="text-background">Sign up</p>
             </RainbowButton>
           </div>
         </div>
-        <div className={`max-md:w-full flex-shrink pointer-events-none`}>
+
+        {/* === IMAGE === */}
+        <div className="flex justify-center w-full max-w-[400px] lg:max-w-full lg:w-3/5 pointer-events-none relative z-0">
           <Image
             isBlurred
             src="/Images/circlePizza.png"
             alt="pizza image"
             width={650}
+            className="w-full h-auto mx-auto"
           />
         </div>
       </section>
+
       <Footer />
     </div>
   );
